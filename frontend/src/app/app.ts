ff6bf2cf9913d32e6e12dacd0738ddd825640032
rakmo33 <omkardabir2013@gmail.com>
2026-05-03 04:35:42 -0700

    add footer styles

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,11 +1,12 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ThemeService } from './core/theme.service';
+import { FooterComponent } from './shared/components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FooterComponent],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
