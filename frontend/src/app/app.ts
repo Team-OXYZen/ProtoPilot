f0dc8756460d2934c7ac0085a728914356e88186
rakmo33 <omkardabir2013@gmail.com>
2026-05-07 13:11:07 -0700

    include full-screen loader

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -2,11 +2,12 @@ import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ThemeService } from './core/theme.service';
 import { FooterComponent } from './shared/components/footer/footer.component';
+import { LoaderComponent } from './shared/components/loader/loader.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, FooterComponent],
+  imports: [RouterOutlet, FooterComponent, LoaderComponent],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
